1e30f8ba5efbf1a17d5e1c9c065b718d7a39a649
Gusti Alfian M. P <gustialfian@gmail.com>
2020-05-21 21:11:23 +0700

    [ADD]: just testing loging

--- mod.ts
+++ mod.ts
@@ -1,5 +1,15 @@
 import { start } from "./src/app.ts";
 
+import { createLogger } from "https://deno.land/x/deno_structured_logging/mod.ts";
+
+const logger = createLogger();
+
+logger.debug("Debug");
+logger.info("Info");
+logger.warn("Warn");
+logger.error("Error");
+logger.critical("Critical");
+
 start().then(() => {
   console.log("server starting");
 });
